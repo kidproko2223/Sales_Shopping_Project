3b72f8f8b4598bf75df5c1d0db5a0324ba17efe2
hainngcd17246 <hainngcd17246@fpt.edu.vn>
2021-04-16 00:47:32 +0700

    Login and Register form

---
+++ app/src/main/java/uk/ac/gre/sales_shopping_project/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package uk.ac.gre.sales_shopping_project.Prevalent;
+
+import uk.ac.gre.sales_shopping_project.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
